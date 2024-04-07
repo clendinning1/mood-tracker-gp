17398c53ae7ab88fed443eead7777889901d4d81
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-04-07 12:53:11 -0500

    added login and mood page routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,4 +5,14 @@ router.get('/', async (req, res) => {
     res.render('homepage');
 });
 
+// login page
+router.get('/login', async (req, res) => {
+    res.render('login');
+});
+
+// mood page
+router.get('/mood', async (req, res) => {
+    res.render('moodpage');
+});
+
 module.exports = router;
