71cb5d277aa68560bf901d2c05750db6ee02df87
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-04-07 13:45:10 -0500

    more sesions todos

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
+const { User } = require('../../models');
 
+// TO DO: post route to grab new user logins
+
+// TO DO: post route to login
+
+// TO DO: post route to logout
 
 
 module.exports = router;
