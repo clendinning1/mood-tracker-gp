05be1f862e71b99e8ff4b61395f56c382a7b5e16
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-04-07 13:36:45 -0500

    cleaned up helpers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,10 +5,19 @@ router.get('/', async (req, res) => {
     res.render('homepage');
 });
 
+
 // login page
 router.get('/login', async (req, res) => {
+    // FOR SESSIONS:
+    // if (req.session.logged_in) {
+    //     // if already logged in, redirect to homepage
+    //     res.redirect('/homepage');
+    //     return;
+    // }
+
     res.render('login');
 });
+
 
 // mood page
 router.get('/mood', async (req, res) => {
