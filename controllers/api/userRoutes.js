bb5d17f5f347b363db21b8f38d96bcc6522622ca
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-04-08 20:19:17 -0500

    have js code, but need to make the basic post route work first

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,25 +1,25 @@
 // routes for user login functionality
-
+// used in public/js/login.js & logout.js
 
 const router = require('express').Router();
 const { User } = require('../../models');
 
 
-// session post route
-// router.post('/', async (req, res) => {
-//     try {
-//         const userData = await User.create(req.body);
+// signup post route
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
 
-//         req.session.save(() => {
-//             req.session.user_id = userData.id;
-//             req.session.logged_in = true;
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
 
-//             res.status(200).json(userData);
-//         });
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// });
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 
 // login post route
@@ -30,7 +30,7 @@ router.post('/login', async (req, res) => {
         if (!userData) {
             res
                 .status(400)
-                .json({ message: 'Incorrect login, try again' });
+                .json({ message: 'Incorrect user, try again' });
             return;
         }
 
