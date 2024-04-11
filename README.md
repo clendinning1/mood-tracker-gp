06e6b2e69048a057e4de24470ddfa812daac865d
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-04-10 23:55:06 -0500

    added website link and openmoji source to readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Mood Tracker
+## Description
+## Usage
+- [Website Link](https://mood-tracker-gp-a6fcbb48bcc3.herokuapp.com/)
+## Sources
+- [Openmoji](https://github.com/hfg-gmuend/openmoji/tree/15.0.0)
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -27,7 +27,6 @@ router.post('/login', async (req, res) => {
     try {
         const userData = await User.findOne({ where: { email: req.body.email } });
 
-
         if (!userData) {
             res
                 .status(400)
