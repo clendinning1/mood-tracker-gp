457d25e09ca4de61d0177ac9e3111269b7a2db18
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-04-10 19:36:27 -0500

    login and logout are working, needed to fix js script links in the handlebars files

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -7,8 +7,11 @@ const { User } = require('../../models');
 
 // signup post route
 router.post('/', async (req, res) => {
+    console.log("signup post route 1");
     try {
         const userData = await User.create(req.body);
+
+        console.log("signup post route 2");
 
         req.session.save(() => {
             req.session.user_id = userData.id;
@@ -24,8 +27,11 @@ router.post('/', async (req, res) => {
 
 // login post route
 router.post('/login', async (req, res) => {
+    console.log("userroutes 0");
     try {
         const userData = await User.findOne({ where: { email: req.body.email } });
+
+        console.log("userroutes 1");
 
         if (!userData) {
             res
@@ -33,6 +39,8 @@ router.post('/login', async (req, res) => {
                 .json({ message: 'Incorrect login, try again' });
             return;
         }
+
+        console.log("userroutes 2");
 
         const validPassword = await userData.checkPassword(req.body.password);
 
@@ -42,6 +50,7 @@ router.post('/login', async (req, res) => {
                 .json({ message: 'Incorrect login, try again' });
             return;
         }
+        console.log("userroutes 3");
 
         req.session.save(() => {
             req.session.user_id = userData.id;
@@ -59,13 +68,17 @@ router.post('/login', async (req, res) => {
 
 // logout post route
 router.post('/logout', (req, res) => {
+    console.log("logout post req 1");
     if (req.session.logged_in) {
+        console.log("logout post req 2");
         req.session.destroy(() => {
             res.status(204).end();
         });
     } else {
+        console.log("logout post req 3");
         res.status(404).end();
     }
+    console.log("logout post req 4");
 });
 
 
--- public/js/logout.js
+++ public/js/logout.js
@@ -3,6 +3,8 @@ const logout = async () => {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
+
+    console.log("logout function");
   
     if (response.ok) {
       document.location.replace('/');
